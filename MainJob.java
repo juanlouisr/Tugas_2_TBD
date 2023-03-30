6dd52b9cebfd9d6a3be23862dfd865e5cb752ff0
juanrombetasik <juan.rombetasik@tiket.com>
2023-03-30 14:11:33 +0700

    update

--- MainJob.java
+++ MainJob.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,6 +15,8 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import scala.Tuple2;
 
 public class MainJob {
@@ -79,9 +82,7 @@ public class MainJob {
     String outputPath = args[1];
 
     // Create a Spark configuration
-    SparkConf conf = new SparkConf()
-        .setAppName("MainJob")
-        .setMaster("local[*]");
+    SparkConf conf = new SparkConf().setAppName("MainJob").setMaster("local[*]");
 
     // Create Spark context
     JavaSparkContext sc = new JavaSparkContext(conf);
@@ -91,30 +92,65 @@ public class MainJob {
 
     // Extract relevant fields and convert to tuples
     JavaPairRDD<Tuple2<String, String>, Integer> tuples = input
-        .flatMapToPair(t -> {
+        .flatMapToPair((PairFlatMapFunction<String, Tuple2<String, String>, Integer>) t -> {
+          ObjectMapper mapper = new ObjectMapper();
           List<Tuple2<Tuple2<String, String>, Integer>> a = new ArrayList<>();
+
           try {
-            ObjectMapper mapper = new ObjectMapper();
             JsonNode rootNode = mapper.readTree(t);
             for (JsonNode j : rootNode) {
+              // Extract the date, social media, and value fields
               String date = constructDate(getDateString(j));
-              if (date != null) {
-                String soc_med = getType(j);
-                a.add(new Tuple2<>(new Tuple2<>(date, soc_med), 1));
+              if (date == null) {
+                continue;
               }
+              String soc_med = getType(j);
+              Integer count = 1;
+              a.add(new Tuple2<>(new Tuple2<>(date, soc_med), count));
             }
+            return a.iterator();
+
           } catch (Exception e) {
             System.out.println("Error reading tree, err val : " + e);
           }
           return a.iterator();
         });
 
-    JavaPairRDD<Tuple2<String, String>, Integer> result = tuples.reduceByKey(Integer::sum);
+    JavaPairRDD<Tuple2<String, String>, Integer> result = tuples
+        .reduceByKey(new Function2<Integer, Integer, Integer>() {
+          private static final long serialVersionUID = 1L;
 
+          @Override
+          public Integer call(Integer v1, Integer v2) {
+            return v1 + v2;
+          }
+        });
+
+    class CustomComparator implements Comparator<Tuple2<String, String>>, Serializable {
+
+      @Override
+      public int compare(Tuple2<String, String> t1, Tuple2<String, String> t2) {
+        try {
+          SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+          Date a = parser.parse(t1._1());
+          Date b = parser.parse(t2._1());
+
+          int res = a.compareTo(b);
+
+          if (res == 0) {
+            return t1._2().compareTo(t2._2());
+          }
+
+          return res;
+        } catch (Exception e) {
+          System.out.println("Failed at parsing dates, doing the string comparison");
+        }
+        return t1._1().compareTo(t2._1());
+      }
+    }
     // SORT
-    JavaPairRDD<Tuple2<String, String>, Integer> sortedResult = result
-        .sortByKey(Comparator.comparing((Tuple2<String, String> t) -> t._1())
-            .thenComparing(Tuple2::_2));
+    JavaPairRDD<Tuple2<String, String>, Integer> sortedResult = result.sortByKey(
+        new CustomComparator());
 
     // Write the sorted results to a text file
     JavaRDD<String> mappedResult = sortedResult.map(
@@ -123,5 +159,4 @@ public class MainJob {
 
     sc.close();
   }
-
 }
